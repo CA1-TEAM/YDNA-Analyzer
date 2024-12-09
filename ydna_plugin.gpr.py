84aaa057ce3aa2cbc262b37b47d15aa23fe67217
CA1-TEAM <CA1T3AM@yahoo.com>
2024-12-09 12:18:23 -0500

    Create ydna_plugin.gpr.py
     Metadata file for Y-DNA Analyzer plugin.

---
+++ ydna_plugin.gpr.py
@@ -0,0 +1,8 @@
+PLUGIN = {
+    "name": "Y-DNA Analyzer",
+    "description": "Analyze Y-DNA data using scatter plots in Gramps.",
+    "version": "1.0",
+    "gramps_target_version": "5.1",
+    "status": "Stable",
+    "fname": "ydna_plugin.py",
+}
